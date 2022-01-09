40140fc4434e78c0bda964a83d082c16651691cc
extremeheat <extreme@protonmail.ch>
2022-01-08 20:30:16 -0500

    Delete mcpc-biomes.js

--- example/mcpc-biomes.js
+++
@@ -1,5 +0,0 @@
-const registry = require('prismarine-registry')('1.18')
-
-registry.biomes.set(2, 'minecraft:ocean', { data: 'about an ocean biome' })
-console.assert(registry.biomes.getIndex('minecraft:ocean') === 2)
-console.log(registry.biomes.get('minecraft:ocean')) // Should get { data: 'about an ocean biome' }
