2fa2f2a9e0ed637d1358d565c03b35fc1dc17f9d
extremeheat <extreme@protonmail.ch>
2022-01-12 18:20:54 -0500

    cleanup

--- test/mcpc.js
+++ test/mcpc.js
@@ -1,6 +1,5 @@
 const collectPackets = require('./util/collectMcpcPackets')
 const Registry = require('prismarine-registry')
-const assert = require('assert')
 
 async function main (version = '1.18') {
   const registry = Registry(version)
@@ -11,12 +10,8 @@ async function main (version = '1.18') {
         registry.loadDimensionCodec(body.dimensionCodec)
         console.log('Loaded dimension codec', registry.biomes)
 
-        const reEncoded = registry.writeDimensionCodec()
-
+        registry.writeDimensionCodec()
         // TODO: Add proper way to compare two NBT objects in prismarine-nbt, as deepEqual will fail with incorrect sorting
-
-        // require('fs').writeFileSync('./dimension-codec0.json', JSON.stringify(reEncoded, null, 2))
-        // require('fs').writeFileSync('./dimension-codec1.json', JSON.stringify(body.dimensionCodec, null, 2))
         // assert.deepEqual(reEncoded, body.dimensionCodec)
         console.log('Re-encoded dimension codec')
       }
