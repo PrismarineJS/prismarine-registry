097414dfaf29f7540f7ca5a58ae9156116ee25d0
Sertonix <uner21@posteo.de>
2022-08-15 19:47:45 +0200

    added additional functions to Registry

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,18 @@
 import {IndexedData} from 'minecraft-data'
+import {NBT} from 'prismarine-nbt'
 
-declare function loader(mcVersion: string): IndexedData
+declare function loader(mcVersion: string): loader.Registry
 declare namespace loader {
-  export type Registry = IndexedData
+  export interface RegistryPc extends IndexedData {
+    loadDimensionCodec( codec: NBT ): void
+    writeDimensionCodec(): NBT
+  }
+  
+  export interface RegistryBedrock extends IndexedData {
+    
+  }
+  
+  export type Registry = RegistryBedrock | RegistryPc
 }
 
 export = loader
