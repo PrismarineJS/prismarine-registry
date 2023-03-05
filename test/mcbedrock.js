ad6e6f6dd2953cdc11f77f9c691c41542a10a31d
CreeperG16 <pappt1216@gmail.com>
2023-03-05 12:53:24 +0100

    Update test for write func, update types, minecraft-bedrock-server (minecraft-wrap doesn't support bedrock yet?)

--- test/mcbedrock.js
+++ test/mcbedrock.js
@@ -1,19 +1,25 @@
 const Registry = require('prismarine-registry')
 const collectPackets = require('./util/collectBedrockPackets')
+// const assert = require('assert')
 
 async function main (version = '1.19.63') {
   const registry = Registry(`bedrock_${version}`)
   let loggedIn = false
   const handlers = {
     play_status (version, params) {
-      if (params.status === 'login_success') {
-        loggedIn = true
-      }
+      if (params.status === 'login_success') loggedIn = true
     },
     start_game (version, params) {
       registry.loadItemStates(params.itemstates)
       console.log('Loaded item palette', registry.items)
-      // console.log('iron shovel', params.itemstates.find(x => x.name === 'minecraft:iron_shovel'), registry.itemsByName.iron_shovel)
+
+      registry.writeItemStates()
+      // TODO: implement component_based so that this works
+      // assert.deepEqual(
+      //   reEncoded.sort((a, b) => a.runtime_id - b.runtime_id),
+      //   params.itemstates.sort((a, b) => a.runtime_id - b.runtime_id)
+      // )
+      console.log('Re-encoded item palette')
     }
   }
 
