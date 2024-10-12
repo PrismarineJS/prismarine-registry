111d9d490f83d577c471ae71ff42b52c0535ec04
Romain Beaumont <romain.rom1@gmail.com>
2024-10-12 17:46:37 +0200

    Increase bedrock timeout

--- test/mcbedrock.test.js
+++ test/mcbedrock.test.js
@@ -4,7 +4,7 @@ const SUPPORTED_VERSIONS = ['1.17.10', '1.18.0', '1.18.11', '1.18.30', '1.19.1',
 const test = require('./mcbedrock')
 
 describe('mcbedrock', function () {
-  this.timeout(9000 * 10)
+  this.timeout(18000 * 10)
 
   for (const version of SUPPORTED_VERSIONS) {
     it('works on ' + version, () => test(version))
